706e114c1dd6faaf25ee3608a8e4159f8e050857
Vadim Opolski <mathmatheart@gmail.com>
2019-06-22 11:55:17 +0300

    Adding hash set

---
+++ src/main/scala/leetcode/my_hash_set/MyHashSet.scala
@@ -0,0 +1,26 @@
+package leetcode.my_hash_set
+
+class MyHashSet() {
+  var map: Map[Int, Int] = Map.empty
+
+  /** Initialize your data structure here. */
+  def add(key: Int): Unit = {
+    map = map + (key -> 1)
+  }
+
+  def remove(key: Int): Unit = {
+    map = map - key
+  }
+
+  /** Returns true if this set contains the specified element */
+  def contains(key: Int): Boolean = map.contains(key)
+
+
+  def main(args: Array[String]): Unit = {
+    var obj = new MyHashSet()
+    obj.add(5)
+    obj.remove(5)
+    var param_3 = obj.contains(5)
+
+  }
+}
